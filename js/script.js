5868fa80c613da75d38ec50e171c98592fc97b92
zilef2 <ajelof2@gmail.com>
2024-06-28 11:22:33 -0500

    hola m

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   });
 
   var typed = new Typed(".typed", {
-    strings: ["Jr. Full-Stack", "Video Editor", "Discord Bot Developer"],
+    strings: ["calculadora?", "Presupuesto", "Developer"],
     typeSpeed: 60,
     startDelay: 900,
     showCursor: false,
